4daf8c8acb7e49c837603621bc676b8b82b7f65f
iarvi <37144235+iarvi@users.noreply.github.com>
2020-09-18 13:08:22 +0530

    Update day10-binary.go

--- 30daysofcode/day10-binary.go
+++ 30daysofcode/day10-binary.go
@@ -1,1 +1,53 @@
+#Author : iArvi
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    var count, maxcount int
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int64(nTemp)
+   // bin := strconv.FormatInt(n, 2)
+    count = 0
+    for n > 0 {
+         r := n % 2
+            n= n/2
+            if(r ==1){
+                count+=1
+                if(count>maxcount){
+                    maxcount = count
+                }
+            }else{
+                count = 0;
+            }
+    }
+    //fmt.Println(bin)
+    fmt.Println(maxcount)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
 
